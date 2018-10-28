e2d532224bdfc8a4c94a1f0085fa28140e764a14
uehara <b1015062@fun.ac.jp>
2018-10-28 11:06:14 +0900

    Fix takePicture bug

--- app/src/main/java/com/example/user/splashnewscam/CameraActivity.kt
+++ app/src/main/java/com/example/user/splashnewscam/CameraActivity.kt
@@ -1,7 +1,6 @@
 package com.example.user.splashnewscam
 
 import android.Manifest
-import android.content.Context
 import android.content.pm.ActivityInfo
 import android.graphics.BitmapFactory
 import android.hardware.Camera
@@ -10,24 +9,23 @@ import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.widget.FrameLayout
-import java.io.IOException
 import java.util.TimerTask
 import java.util.Timer
 import okhttp3.*
 
 
-
-class CameraActivity: AppCompatActivity() {
+class CameraActivity : AppCompatActivity() {
 
     private var mCamera: Camera? = null
     private var mPreview: CameraPreview? = null
+    var taking = false
+    var mTimer: Timer? = null
 
     fun getCameraInstance(): Camera? {
         return try {
             Camera.open() // attempt to get a Camera instance
         } catch (e: Exception) {
             // Camera is not available (in use or does not exist)
-            Log.d("hoge", "fuga")
             null // returns null if camera is unavailable
         }
     }
@@ -40,6 +38,7 @@ class CameraActivity: AppCompatActivity() {
         val fileName = "testfile.jpg"
         val bitmap = BitmapFactory.decodeByteArray(data, 0, data.size)
 
+        /*
         try {
             openFileOutput(fileName,
                     Context.MODE_PRIVATE).use { fileOutputstream ->
@@ -50,25 +49,36 @@ class CameraActivity: AppCompatActivity() {
         } catch (e: IOException) {
             e.printStackTrace()
         }
+        */
 
 
     }
 
-    private val mPicture = Camera.PictureCallback { data, _ ->
+    val mPicture = Camera.PictureCallback { data, _ ->
         Log.d("data", data.size.toString())
         saveImg(data)
         mCamera?.startPreview()
     }
 
+    public fun takePicture() {
+
+        try {
+            mCamera?.takePicture(null, null, mPicture)
+        }catch (e: java.lang.Exception) {
+            Log.d("tag", e.toString())
+        }
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        requestedOrientation =ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
 
         setContentView(R.layout.activity_camera)
 
         ActivityCompat.requestPermissions(this,
-                arrayOf(Manifest.permission.CAMERA), 1234)
+                arrayOf(Manifest.permission.CAMERA), 12345)
 
         // Create an instance of Camera
         mCamera = getCameraInstance()
@@ -84,19 +94,34 @@ class CameraActivity: AppCompatActivity() {
             preview.addView(it)
         }
 
-        val timer = Timer(true)
+        mTimer = Timer()
         val handler = android.os.Handler()
-        timer.schedule(
+        mTimer?.schedule(
                 object : TimerTask() {
                     override fun run() {
                         handler.post {
                             // get an image from the camera
-                            mCamera?.takePicture(null, null, mPicture)
+                            takePicture()
                         }
                     }
-                }, 3000, 5000   //開始遅延(何ミリ秒後に開始するか)と、周期(何ミリ秒ごとに実行するか)
+                }, 3000, 10000   //開始遅延(何ミリ秒後に開始するか)と、周期(何ミリ秒ごとに実行するか)
         )
+        val mPicture = Camera.PictureCallback { data, _ ->
+            Log.d("data", data.size.toString())
+            // saveImg(data)
+        }
+        if (!taking) {
+            taking = true
+            try {
+                mCamera?.takePicture(null, null, mPicture)
+            }catch (e: java.lang.Exception) {
 
+            }
+        }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        mTimer?.cancel()
     }
 }
